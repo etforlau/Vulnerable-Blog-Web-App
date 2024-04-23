7cdee044423122f513d19013081570c3800196a5
ricardo-0103 <ricardoarturonunez2001@gmail.com>
2024-04-23 17:51:07 -0500

    Creación de blogs, blogs de ejemplo, estructura página principal y página de blogs

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
+app.get("/blogs", (req, res) => {
+  res.render("blogsPage.ejs", { blogs: blogs });
+});
+
+app.post("/createBlog", (req, res) => {
+  const newBlog = {
+    title: req.body["title"],
+    author: req.body["author"],
+    content: req.body["content"],
+    date: `${date.toLocaleString("en", {
+      month: "long",
+    })} ${date.getDate()}, ${date.getFullYear()}`,
+  };
+  blogs.push(newBlog);
+  res.render("blogsPage.ejs", { blogs: blogs });
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
@@ -19,8 +36,18 @@ const date = new Date();
 
 const blogs = [];
 blogs.push({
-  title: "Blog example",
+  title: "Blog example 1",
   author: "Ricardo Núñez",
+  content:
+    "Discover the world of blogging with our platform. Share your stories, experiences, and insights with a supportive community of writers and readers. Join us today and start creating engaging content that resonates with others.",
+  date: `${date.toLocaleString("en", {
+    month: "long",
+  })} ${date.getDate()}, ${date.getFullYear()}`,
+});
+
+blogs.push({
+  title: "Blog example 2",
+  author: "Antonio Lopez",
   content:
     "Discover the world of blogging with our platform. Share your stories, experiences, and insights with a supportive community of writers and readers. Join us today and start creating engaging content that resonates with others.",
   date: `${date.toLocaleString("en", {
---
+++ public/main.js
@@ -0,0 +1,4 @@
+document.querySelector(".btn_container").addEventListener("click", () => {
+  const blog_container = document.querySelector(".create_task");
+  blog_container.style.display = "flex";
+});
